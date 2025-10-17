da4e9212e9e5311d5fe9a700d12502da5219d3b1
NguyenHuuHung <nguyenhuuhung.9a10.2013@gmail.com>
2025-10-17 09:46:24 +0700

    homeword deadline 17/10/2025

---
+++ homework/17-10-2025/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+Func<string> findLongestWord = () =>
+{
+    string input = Console.ReadLine() ?? string.Empty;
+    string[] words = input.Trim().Split(' ');
+    int maxLength = 0;
+    string result = "";
+    foreach (string word in words)
+    {
+        if (word.Length > maxLength)
+        {
+            result = word;
+            maxLength = word.Length;
+        }
+    }
+    return result;
+};
+
+Console.WriteLine($"Longest word is {findLongestWord()}");
+
+
+Func<string> removeSpecialCharacter = () =>
+{
+    string input = Console.ReadLine() ?? String.Empty;
+    return Regex.Replace(input, @"[^a-zA-Z0-9\s]", "");
+};
+
+
+Console.WriteLine($"Word after removing special character: {removeSpecialCharacter()}");
+
+
+
+Func<string> ReturnLongestStringWithNumber = () =>
+{
+    string input = Console.ReadLine() ?? string.Empty;
+    input = input.Trim();
+
+    int currentLength = 0;
+    string result = string.Empty;
+    StringBuilder sb = new StringBuilder();
+    bool containsNumber = false;
+
+    foreach (char c in input)
+    {
+        if (c != ' ')
+        {
+            sb.Append(c);
+            if (char.IsNumber(c))
+                containsNumber = true;
+        }
+        else
+        {
+            if (sb.Length > 0)
+            {
+                if (containsNumber && sb.Length > currentLength)
+                {
+                    result = sb.ToString();
+                    currentLength = sb.Length;
+                }
+                sb.Clear();
+                containsNumber = false;
+            }
+        }
+    }
+
+    if (sb.Length > 0)
+    {
+        if (containsNumber && sb.Length > currentLength)
+        {
+            result = sb.ToString();
+        }
+    }
+    return result;
+};
+
+Console.WriteLine($"Output: {ReturnLongestStringWithNumber()}");
---
+++ homework/21-10-2025/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+
+List<int> lstNumber = [20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20];
+
+// Tính tổng các số lớn hơn 50 trong danh sách
+Func<List<int>, int> totalNumberGreaterThan50 = arr =>
+{
+    int total = 0;
+    foreach (int num in arr)
+    {
+        if (num > 50) total += num;
+    }
+    return total;
+};
+
+Console.WriteLine($@"Total of numbers greater than 50 = {totalNumberGreaterThan50(lstNumber)}");
+
+// Tính số phần tử lớn hơn 30
+Func<List<int>, int> calculateNumberGreaterThan30 = arr =>
+{
+    int total = 0;
+    foreach (int num in arr)
+    {
+        if (num > 30) total += 1;
+    }
+    return total;
+};
+Console.WriteLine($@"Total of numbers greater than 30 = {calculateNumberGreaterThan30(lstNumber)}");
+
+int max = int.MinValue;
+Action<List<int>> findMax = arr =>
+{
+    foreach (int num in arr)
+    {
+        if (num > max) max = num;
+    }
+};
+findMax(lstNumber);
+Console.WriteLine($@"Max number is = {max}");
+
+
+double average = 0;
+Func<List<int>, double> averageOfOddNumber = arr =>
+{
+    foreach (int num in arr)
+    {
+        average += num;
+    }
+    return (double)average / arr.Count;
+};
+
+Console.WriteLine($@"Average odd numbers is = {averageOfOddNumber(lstNumber)}");
